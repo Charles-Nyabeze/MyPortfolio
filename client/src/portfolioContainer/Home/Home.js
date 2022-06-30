b548f3264e720c6402d2c70a1f0252c9bef91b2c
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-06-30 16:17:22 -0400

    starter comp

--- client/src/portfolioContainer/Home/Home.js
+++ client/src/portfolioContainer/Home/Home.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import Profile from './Profile/Profile'
-import Footer from './Footer/Footer'
-import Header from './Header/Header'
-import './Home.css'
+import React from "react";
+import Profile from "./Profile/Profile";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
 
-export default function Home() {
+import "./Home.css";
+
+export default function Home(props) {
   return (
-    <div className='home-container'>
-         <Header/>
-         <Profile/>
-         <Footer/>
-
+    <div className="home-container" id={props.id || ""}>
+      <Header />
+      <Profile />
+      <Footer />
     </div>
-  )
+  );
 }
