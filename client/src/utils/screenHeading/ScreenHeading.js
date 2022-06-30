a7761e8c246a0a163ba2d78e284d08e11ed62bae
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-06-30 01:40:24 -0400

    header/scroll

---
+++ client/src/utils/screenHeading/ScreenHeading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ScreenHeading(props) {
+  return (
+    <div className='heading-container'>
+        <div className='screen-heading'>
+            <span>{props.title}</span>
+
+        </div>
+        
+    </div>
+  )
+}
